215605b31840f15a876930746326d609a5d0d2be
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-06-26 18:12:14 -0400

    This is my second edit to my Javascript file. My first task is to use javascript to generate a random word from an array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+/*var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z']
+        't', 'u', 'v', 'w', 'x', 'y', 'z']*/
+        var things = ['Rock', 'Paper', 'Scissor'];
+var thing = things[Math.floor(Math.random()*things.length)];
+alert('The computer chose:' + thing);
